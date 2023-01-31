54e5116f907adc1f712f6179a8414cb2a80e43cd
Nate Dunn <hello@natedunn.net>
2023-01-30 20:42:36 -0600

    Add next few pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: import.meta.env.PUBLIC_PROD_URL,
   integrations: [
+    mdx(),
     tailwind(),
-    preact(),
+    preact({
+      compat: true,
+    }),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,16 @@
 import { defineCollection } from 'astro:content';
 import { z } from 'zod';
 
-const exampleCollection = defineCollection({
+const postsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
+    date: z.string(),
+    description: z.string(),
   }),
 });
 
 export const collections = {
-  example: exampleCollection,
+  posts: postsCollection,
 };
